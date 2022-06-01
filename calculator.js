749c15d983cdc24746333dfb80cbad8958424d02
shiela <sheshemarie31@gmail.com>
2022-06-01 15:17:49 +0800

    Round off result to 3 decimals to not overflow the screen with long decimals.

--- calculator.js
+++ calculator.js
@@ -75,4 +75,9 @@ const deleteBtn = document.querySelector('#delete');
  //To Calculate
  function calculate() {
   result = operate(clickedOperator, parseFloat(firstNumber), parseFloat(storedNumber));
+  //round off numbers to 3 decimal; converted to string for the split() method works; limit teh result to 21 numbers to nto overflow screen;
+  roundedResult = Number(result.toFixed(3)).toString().substring(0, 21);
+  currentNum.textContent = roundedResult;
+  previousNum.textContent = `${firstNumber} ${clickedOperator} ${storedNumber}`;
+  storedNumber = roundedResult;
  }
