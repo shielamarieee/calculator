b34fb1d9e07768b3a92795a0f471fbd647200b40
shiela <sheshemarie31@gmail.com>
2022-06-01 14:35:03 +0800

    Create a function taht takes an operator and 2 numbers to execute the math operator functions

--- calculator.js
+++ calculator.js
@@ -14,3 +14,15 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b
 }
+
+function operate(operator, num1, num2) {
+  if(operator === '+') {
+    return add(num1, num2);
+  } else if (operator === '-') {
+    return subtract(num1, num2);
+  } else if (operator === 'x') {
+    return multiply(num1, num2);
+  } else if (operator === '÷' ) {
+    return divide(num1, num2);
+  }
+}
